6a9d517f9609d883ac92533f992455e966ed45bd
Stefan Sommer <tonprince@gmail.com>
2023-06-02 15:30:05 +0700

    added new

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { createBoundary } from './ErrorBoundary.js';
+    import FirestackError from './FirestackError.svelte';
+  
+    let Boundary = createBoundary(FirestackError);
+  
+    function getUsers() {
+      return ["aaa", "sss"];
+    }
+    </script>
+  
+  <Boundary>
+    {#await getUsers() then users}
+      {user.error()}
+    {/await}
+  </Boundary>
---
+++ src/routes/FirestackError.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { dev } from '$app/environment';
+
+  export let error = null;
+</script>
+
+{#if $error}
+  <div class="container p-4 is-flex is-flex-direction-column">
+    <div class="content">
+      <h1 class="title is-1 has-text-danger">{$error.message}</h1>
+    </div>
+
+    <div class="error">
+      <pre class="trace">
+      {dev ? $error.stack : ''}
+    </pre>
+    </div>
+  </div>
+{:else}
+  <slot />
+{/if}
+
+<style>
+  .error {
+    border: 1px solid red;
+  }
+  .trace {
+    font-family: monospace;
+  }
+</style>
