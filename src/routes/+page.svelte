286cbc8d236480c69cb1c4ec6c2188db2c7d45f4
Stefan Sommer <tonprince@gmail.com>
2023-06-02 15:47:15 +0700

    fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
   import { createBoundary } from './ErrorBoundary.js';
-    import FirestackError from './FirestackError.svelte';
-  
-    let Boundary = createBoundary(FirestackError);
-  
-    async function getUsers() {
-      return ["aaa", "sss"];
-    }
-    </script>
-  
-  <Boundary>
-    {#await getUsers() then users}
-      {users.error()}
-    {/await}
-  </Boundary>
+  import FirestackError from './FirestackError.svelte';
+
+  let Boundary = createBoundary(FirestackError);
+
+  async function getUsers() {
+    return ['aaa', 'sss'];
+  }
+</script>
+
+<Boundary>
+  {#await getUsers() then users}
+    {users.error()}
+  {/await}
+</Boundary>
